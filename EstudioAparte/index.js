2eff2cfe721f128a236b32ffc10ecf7d4fca2d36
CiroFlorentin <cfloren01@gmail.com>
2025-08-25 23:22:11 -0300

    cambia la lógica de manipulación del array de juegos: reemplaza push por unshift para agregar "Sonic" y añade la eliminación de un juego con pop

--- EstudioAparte/index.js
+++ EstudioAparte/index.js
@@ -8,5 +8,8 @@ juegos.forEach((elemento, indice) => {
 let nuevaLongitud = juegos.push("Final Fantasy");
 console.log({ nuevaLongitud, juegos });
 
-let nuevaLongitud2 = juegos.push("Sonic");
+let nuevaLongitud2 = juegos.unshift("Sonic");
 console.log({ nuevaLongitud2, juegos });
+
+let juegoBorrado = juegos.pop();
+console.log({ juegoBorrado, juegos });
