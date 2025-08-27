4bf4e8e63d615e33c8ca3156c35cc2ba79765cb6
CiroFlorentin <cfloren01@gmail.com>
2025-08-27 20:13:08 -0300

    agrega ejemplos de funciones de orden superior y su uso

--- Paradigma/Clases/2da-clase.js
+++ Paradigma/Clases/2da-clase.js
@@ -7,3 +7,26 @@ const esMenorQue10 = (x) => x < 10;
 const esParMayor = and(esPar, esMenorQue10);
 console.log(esParMayor(8)); // true
 console.log(esParMayor(12)); // false
+
+//? Funciones de orden superior
+
+const operar = (n, f) => (x) => f(n) + x;
+
+const moverDerecha = (x) => x + 1;
+const moverIzquierda = (x) => x - 1;
+
+const mx = operar(5, moverDerecha);
+const mi = operar(5, moverIzquierda);
+
+console.log(mx(10)); // 16
+console.log(mi(10)); // 14
+
+//! Funciones de orden superior
+const f2 = (n) => n + 1;
+const f1 = (x, y, f) => x + y + f(x);
+console.log(f1(1, 2, f2)); // 4
+
+//* Funciones de orden superior
+const suma = (x) => x + 1;
+const duplicar = (f) => (x) => f(f(x)); //duplicar vuelve a aplicar la función f dos veces, en este caso suma
+console.log(duplicar(suma)(10)); // 12
